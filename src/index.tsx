bcb1d16cfc7ef57c1ac8234397dea40800a0810b
liangyue <nvcsadjkojf@outlook.com>
2021-06-15 17:56:35 +0800

    update: add config for react + webpack + swc loader

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom'
+
+const App = () => <div>app</div>
+
+ReactDOM.render(<App />, document.querySelector('#root'))
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+const mode = process.env.mode || 'development'
+const filename = mode === 'development' ? '[name].js' : '[name].[contenthash:7].js'
+const sourceMap =
+  mode === 'development' ? 'eval-source-map' : 'nosources-source-map'
+
+module.exports = {
+  mode,
+  devtool: sourceMap,
+  resolve: {
+    // modules: ['node_modules', 'src'],
+    extensions: ['.tsx', '.js'],
+  },
+  devServer: { historyApiFallback: true, port: process.env.PORT || '8080' },
+  entry: './src/index.tsx',
+  output: {
+    filename,
+    clean: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({ template: './src/assets/index.html' }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        use: {
+          loader: 'swc-loader',
+          options: {
+            sync: true,
+            jsc: {
+              parser: { syntax: 'typescript', tsx: true, dynamicImport: true },
+              transform: { react: { runtime: 'automatic' } },
+            },
+          },
+        },
+      },
+    ]
+  }
+}
