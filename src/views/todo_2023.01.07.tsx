7f8ab0c40b3c995f43216d412e24c241a54f3008
小树 <haishu.liu@sunmi.com>
2023-01-07 18:24:22 +0800

    feat: support mobile device screen

--- src/views/todo_2023.01.07.tsx
+++ src/views/todo_2023.01.07.tsx
@@ -27,46 +27,49 @@ export default () => {
   return (
     <div className="container mx-auto h-screen flex flex-col">
       <div className="mt-4 text-3xl text-center flex-none">todos</div>
-      <div className="mt-12 p-4 flex-auto flex flex-col overflow-hidden">
-        <div className="flex gap-4 flex-none">
+      <div className="mt-4 md:mt-12 p-4 flex-auto flex flex-col overflow-hidden">
+        <div className="flex flex-none flex-wrap gap-4">
           <input
             value={input}
             onChange={(e) => setInput(e.target.value)}
             onKeyDown={handleKeyDown}
-            className="border p-2 mr-10 w-60 flex-auto"
+            className="w-full md:w-80 border p-2"
             placeholder="What are you want to do?"
           />
-          <button
-            className={clsx("border px-6 rounded-lg", {
-              "border-blue-500": store.filter === "unchecked",
-              "border-blue-200": store.filter !== "unchecked",
-            })}
-            onClick={() =>
-              store.filter !== "unchecked" && store.setFilter("unchecked")
-            }
-          >
-            unchecked
-          </button>
-          <button
-            className={clsx("border px-6 rounded-lg", {
-              "border-blue-500": store.filter === "checked",
-              "border-blue-200": store.filter !== "checked",
-            })}
-            onClick={() =>
-              store.filter !== "checked" && store.setFilter("checked")
-            }
-          >
-            checked
-          </button>
-          <button
-            className={clsx("border px-6 rounded-lg", {
-              "border-blue-500": store.filter === "all",
-              "border-blue-200": store.filter !== "all",
-            })}
-            onClick={() => store.filter !== "all" && store.setFilter("all")}
-          >
-            all
-          </button>
+          <div className="flex-auto"></div>
+          <div className="flex-none flex items-center gap-4 md:ml-10">
+            <button
+              className={clsx("border px-4 md:px-6 rounded-lg h-10", {
+                "border-blue-500": store.filter === "unchecked",
+                "border-gray-500": store.filter !== "unchecked",
+              })}
+              onClick={() =>
+                store.filter !== "unchecked" && store.setFilter("unchecked")
+              }
+            >
+              unchecked
+            </button>
+            <button
+              className={clsx("border px-4 md:px-6 rounded-lg h-10", {
+                "border-blue-500": store.filter === "checked",
+                "border-gray-500": store.filter !== "checked",
+              })}
+              onClick={() =>
+                store.filter !== "checked" && store.setFilter("checked")
+              }
+            >
+              checked
+            </button>
+            <button
+              className={clsx("border px-4 md:px-6 rounded-lg h-10", {
+                "border-blue-500": store.filter === "all",
+                "border-gray-500": store.filter !== "all",
+              })}
+              onClick={() => store.filter !== "all" && store.setFilter("all")}
+            >
+              all
+            </button>
+          </div>
         </div>
         <ul className="mt-4 p-4 flex-auto shadow-lg overflow-auto">
           {store.list?.map((i) => (
@@ -86,7 +89,7 @@ export default () => {
               <span className="flex-auto break-all">{i.content}</span>
               <div
                 onClick={() => del({ id: i.id })}
-                className="cursor-pointer flex-none text-red-500 ml-2 w-8 h-8 overflow-hidden flex justify-center align-middle"
+                className="cursor-pointer flex-none text-red-500 ml-2 w-8 h-8 overflow-hidden flex justify-center items-center"
               >
                 <div className="rotate-45 origin-center">+</div>
               </div>
